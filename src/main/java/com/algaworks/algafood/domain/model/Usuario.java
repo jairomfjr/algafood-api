37f762ef576971ac9de47ce1edf02886a5267906
jairo.junior <jairo.junior@sps.ce.gov.br>
2022-06-18 16:51:17 -0300

    Correção no model de usuario

--- src/main/java/com/algaworks/algafood/domain/model/Usuario.java
+++ src/main/java/com/algaworks/algafood/domain/model/Usuario.java
@@ -22,28 +22,28 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Usuario {
-	
+
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@Column(nullable = false)
-    private String nome;
-    
-    @Column(nullable = false)
-    private String email;
-    
-    @Column(nullable = false)
-    private String senha;
-    
-    @CreationTimestamp
-    @Column(nullable = false, columnDefinition = "datetime")
-    private LocalDateTime dataCadastro;
-    
-    @ManyToMany
-    @JoinTable(name = "usuario_grupo",
-    		joinColumns = @JoinColumn(name = "usuario_id"),
-    		inverseJoinColumns = @JoinColumn(name = "grupo_id"))
-    private List<Grupo> grupos = new ArrayList<>();
+	private String nome;
+
+	@Column(nullable = false)
+	private String email;
+
+	@Column(nullable = false)
+	private String senha;
+
+	@CreationTimestamp
+	@Column(nullable = false, columnDefinition = "datetime")
+	private LocalDateTime dataCadastro;
+
+	@ManyToMany
+	@JoinTable(name = "usuario_grupo", 
+		joinColumns = @JoinColumn(name = "usuario_id"), 
+		inverseJoinColumns = @JoinColumn(name = "grupo_id"))
+	private List<Grupo> grupos = new ArrayList<>();
 }
