5813af9c5eb5b1a44e0e62723dd6d8d0a593270b
jairo.junior <jairo.junior@sps.ce.gov.br>
2022-09-04 13:39:33 -0300

    Implementação de novas funcionalidades e criação de novas exceptions

---
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -0,0 +1,21 @@
+package com.algaworks.algafood.api.exceptionhandler;
+
+import lombok.Getter;
+
+@Getter
+public enum ProblemType {
+	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível"),
+	RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado"),
+	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
+	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
+	PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"),
+	ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema");
+
+	private String title;
+	private String uri;
+
+	ProblemType(String path, String title) {
+		this.uri = "https://algafood.com.br".concat(path);
+		this.title = title;
+	}
+}
