89a10ea71ffac3f0910fdbbc8dd1f3e3bba34be8
Jairo Junior <jairo.junior@sps.ce.gov.br>
2022-09-19 16:31:37 -0300

    Novas implementações para melhorar as mensagens da classe ApiExceptionHandler

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.api.exceptionhandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -15,8 +16,15 @@ public class Problem {
 	private Integer status;
 	private String type;
 	private String title;
-	private String detail;
-	
+	private String detail;	
 	private String userMessage;
 	private LocalDateTime timestamp;
+	private List<Field> fields;
+	
+	@Getter
+	@Builder
+	public static class Field {
+		private String name;
+		private String userMessage;
+	}
 }
