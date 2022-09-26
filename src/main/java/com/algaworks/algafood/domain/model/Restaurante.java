a0c8b66e9d7893c72ea538ee2574d8ac216798af
Jairo Junior <jairo.junior@sps.ce.gov.br>
2022-09-26 14:55:13 -0300

    Constraints de validação

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -19,6 +19,7 @@ import javax.persistence.OneToMany;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
 import javax.validation.groups.ConvertGroup;
 import javax.validation.groups.Default;
 
@@ -46,6 +47,8 @@ public class Restaurante {
 	private String nome;
 
 	@NotNull
+	@PositiveOrZero
+	//@Multiplo(numero = 5)
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 	
